688ba3fceb6453b03d743d7721f2007990f2e4bc
yelliboinakusuma01 <yelliboinakusuma2004@gmail.com>
2025-06-27 17:43:27 +0530

    server side

---
+++ Project files - server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectionofDb = require("./config/connect.js");
+const path = require("path");
+
+const app = express();
+
+//////dotenv config/////////////////////
+dotenv.config();
+
+//////connection to DB/////////////////
+connectionofDb();
+
+
+const PORT = process.env.PORT || 8001;
+
+
+app.use(express.json());
+app.use(cors({
+  origin:'http://localhost:3000',
+  methods:['GET','POST']
+}));
+
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use('/api/user', require('./routes/userRoutes.js'))
+app.use('/api/admin', require('./routes/adminRoutes'))
+app.use('/api/owner', require('./routes/ownerRoutes'))
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
